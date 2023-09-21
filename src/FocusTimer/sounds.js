f3555554ad2263aae8d718edf6be4ea2da1ce28f
Beatriz <104030147+beatriz-gm@users.noreply.github.com>
2023-09-21 15:53:19 -0300

    Update sounds.js

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -3,3 +3,5 @@ export const buttonPressAudio = new Audio('./assets/sounds/buttonPress.wav')
 export const timerMusic = new Audio('./assets/sounds/theImperialMarch.mp3')
 
 export const kichenTimer = new Audio('./assets/sounds/kichenTimer.mp3')
+
+timerMusic.loop = true
